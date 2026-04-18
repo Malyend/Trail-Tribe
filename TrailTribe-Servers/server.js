5ad25bbd9ac14ec842928ea42a01f183c7fe2110
malyend <191012129+Malyend@users.noreply.github.com.>
2026-04-18 20:52:43 +0100

    Started working on the server/backend.
    Created backend using node.js and express.
    
    Touched on index css.
    The footer specifically.
    
    setting it up now.
    What else----
    Not sure.

---
+++ TrailTribe-Servers/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+// postgres Database here
+
+//get all needed components
+const webPush = require('web-push');
+const express = require('express');
+const cors = require('cors');
+const cron = require('node-cron');
+
+//Vapid Keys here
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//user Subscription
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
