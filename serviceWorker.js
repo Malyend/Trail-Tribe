195119005205b3506d5d2e70069811c70e4ac962
malyend <191012129+Malyend@users.noreply.github.com.>
2026-04-22 22:35:31 +0100

    added more cached files

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,15 +1,21 @@
 
 self.addEventListener('install', (event) => {
-    self.skipwaiting();
     event.waitUntil(
         caches.open('Trail-v1').then((cache) => {
             return cache.addAll([
                 './',
+
                 './index.html',
                 './Contact.html',
                 './Charity.html',
                 './Videos.html',
+
                 './Trail.js',
+                './trail-charity.js',
+                './trail-Contact.js',
+                './trail-merch.js',
+                './trail.video.js',
+
                 './tribecss.css'
             ])
         })
